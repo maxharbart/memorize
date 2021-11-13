74eccee6d44497dcde2309a1cf8022a490775ae1
max <max>
2021-11-13 03:36:22 +0200

    Initial Commit

---
+++ Memorize!/Memorize_App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_App.swift
+//  Memorize!
+//
+//  Created by Максим Гарбарт on 13.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
